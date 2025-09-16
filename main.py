db71f2edd331a93ba307c28b4d7cfe62c9e8efe6
Andreas Nilsson <andreas.nilsson@hkr.se>
2025-09-16 23:22:41 +0200

    Refactor ANSI color management to use a dictionary instead of importing Ansi class

--- main.py
+++ main.py
@@ -2,12 +2,28 @@
 
 import random
 from time import sleep
-from ansi import Ansi
+
+# ANSI escape sequences as a dictionary
+ANSI = {
+    'HIDE_CURSOR': '\033[?25l',
+    'SHOW_CURSOR': '\033[?25h',
+    'CLEAR_SCREEN': '\033[2J',
+    'CURSOR_HOME': '\033[H',
+    'RESET': '\033[0m',
+    'GREEN': '\033[32m',
+    'YELLOW': '\033[33m',
+    'LIGHT_COLORS': [
+        '\033[31m',  # red
+        '\033[33m',  # yellow
+        '\033[35m',  # magenta
+        '\033[36m'   # cyan
+    ]
+}
 
 
 def get_tree():
     """Return a green tree template as a string."""
-    tree = Ansi.GREEN
+    tree = ANSI['GREEN']
     tree += r'''
        *
       / \
@@ -21,7 +37,7 @@ def get_tree():
 /_____________\
       |||
 '''
-    tree += Ansi.RESET
+    tree += ANSI['RESET']
     return tree
 
 
@@ -32,19 +48,19 @@ def colorize_tree(tree):
     star_index = tree_list.index('*')
 
     for i in ornament_indices:
-        tree_list[i] = f'{random.choice(Ansi.LIGHT_COLORS)}o{Ansi.GREEN}'
-    tree_list[star_index] = f'{Ansi.YELLOW}*{Ansi.GREEN}'
+        tree_list[i] = f"{random.choice(ANSI['LIGHT_COLORS'])}o{ANSI['GREEN']}"
+    tree_list[star_index] = f"{ANSI['YELLOW']}*{ANSI['GREEN']}"
 
     return ''.join(tree_list)
 
 
 def clear_screen():
     """Clear the screen and place the cursor at the top left."""
-    print(Ansi.CLEAR_SCREEN + Ansi.CURSOR_HOME, end='')
+    print(ANSI['CLEAR_SCREEN'] + ANSI['CURSOR_HOME'], end='')
 
 
 def main():
-    print(Ansi.HIDE_CURSOR)
+    print(ANSI['HIDE_CURSOR'])
     for _ in range(10):
         clear_screen()
         colorized_tree = colorize_tree(get_tree())
@@ -52,7 +68,7 @@ def main():
         sleep(0.5)
 
     clear_screen()
-    print(Ansi.SHOW_CURSOR)
+    print(ANSI['SHOW_CURSOR'])
 
 
 if __name__ == '__main__':
